003a135538b8e2b27ff123a7cda2c45e9f218fca
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2025-09-12 20:41:50 -0300

    initial commit

---
+++ src/test/java/com/medsync/historico/HistoricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medsync.historico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
