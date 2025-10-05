690d67dd316a840cd62ca5e6c637c1445af5c6b7
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2025-10-04 22:45:01 -0300

    feat: use response status annotation in resolver

--- src/main/java/com/medsync/historico/application/exceptions/AppointmentNotFoundException.java
+++ src/main/java/com/medsync/historico/application/exceptions/AppointmentNotFoundException.java
@@ -1,5 +1,9 @@
 package com.medsync.historico.application.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class AppointmentNotFoundException extends RuntimeException {
 
     public AppointmentNotFoundException(Long id) {
